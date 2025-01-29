bf001dd3a98ee64559f368b411a9fa9dfa138500
rahoolkar <batman30jan2002@gmail.com>
2025-01-29 12:55:30 +0530

    passing multiple middleware

--- middleware.js
+++ middleware.js
@@ -36,6 +36,40 @@ app.get("/",(req,res)=>{
     res.send("get request recieved")
 })
 
+app.use("/new",function(req,res,next){
+    console.log("hi, i am middleware only for /new route");
+    next();
+})
+
+app.get("/new",(req,res)=>{
+    res.send("new route me entry")
+})
+
+//middleware for only /api route
+const middleware1 = function(req,res,next){
+    console.log("first middleeare of the /api route");
+    next();
+}
+
+const middleware2 = function(req,res,next){
+    let{query} = req.query;
+    if(query=="jotaro"){
+        next();
+    }else{
+        res.send("nhi milega data, bhaag yaha se");
+    }
+}
+
+//we can pass the middleware as argument in the app.get function to run the middleware before reaching to the /api get route 
+app.get("/api",middleware1,middleware2,function(req,res){
+    res.send("api data lelo");
+})
+
+//404 //if the above doesnot match with any route then this middleware will run
+app.use(function(req,res){
+    res.send("error 404 page not found")
+})
+
 app.listen(8080,()=>{
     console.log("app is running on port 8080");
 })
