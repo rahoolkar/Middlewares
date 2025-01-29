01bb8dc9fa7c0660489580d4d757c85e23f3e945
rahoolkar <batman30jan2002@gmail.com>
2025-01-29 12:21:56 +0530

    logger middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express;
+const app = express();
 
 //Middlewares the functions that are executed when the request is recieved or before the response is send
 
@@ -9,11 +9,27 @@ const app = express;
 //Middlewares can call the nect Middleware can form a chain like this
 //Middleware can send the response 
 
-//Defining a Middleware
-app.use(function(req,res){
-    console.log("hi, i am a middleware") //this will work for every request route
+//Defining a Middleware //middleware-one
+app.use(function(req,res,next){
+    console.log("Time:",new Date(Date.now()).toDateString()) //this will work for every request route
+    console.log("hi, i am first middleware"); //middleware-one
     //Middleware have two choices either it can a send a response from here or it can call another next()
-    
+    next(); //next middleware ko run karega 
+})
+
+//middleware-two
+app.use((req,res,next)=>{
+    console.log("Time:", new Date(Date.now()).toDateString());
+    console.log("hi, i am middleware-two");
+    next(); //route ko call karega
+
+})
+
+//Logger Middleware //middleware-three
+app.use(function(req,res,next){
+    req.inTime = new Date(Date.now()).toString();
+    console.log(req.method,req.inTime,req.path,req.hostname)
+    next();
 })
 
 app.get("/",(req,res)=>{
